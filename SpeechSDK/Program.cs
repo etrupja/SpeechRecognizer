35d3a8a848bdfc0148c93ea6e944e25dca1d296e
ERVIS-PC\Ervis <ervistrupja@outlook.com>
2018-11-23 20:44:10 +0100

    Get your-subscription-key-here

--- SpeechSDK/Program.cs
+++ SpeechSDK/Program.cs
@@ -15,39 +15,37 @@ namespace SpeechSDK
 
         public static async Task RecognizeSpeechAsync()
         {
-            // Creates an instance of a speech config with specified subscription key and service region.
-            // Replace with your own subscription key // and service region (e.g., "westus").
-            var config = SpeechConfig.FromSubscription("92ad117fab2246ed9e65166417ba98cf", "westus");
+            //Get your-subscription-key-here : https://azure.microsoft.com/en-us/try/cognitive-services/my-apis/
+            var config = SpeechConfig.FromSubscription("your-subscription-key-here", "westus");
 
             // Creates a speech recognizer.
             using (var recognizer = new SpeechRecognizer(config))
             {
                 Console.WriteLine("Say something...");
-
-                // Performs recognition. RecognizeOnceAsync() returns when the first utterance has been recognized,
-                // so it is suitable only for single shot recognition like command or query. For long-running
-                // recognition, use StartContinuousRecognitionAsync() instead.
                 var result = await recognizer.RecognizeOnceAsync();
 
-                // Checks result.
-                if (result.Reason == ResultReason.RecognizedSpeech)
+                switch (result.Reason)
                 {
-                    Console.WriteLine($"We recognized: {result.Text}");
-                }
-                else if (result.Reason == ResultReason.NoMatch)
-                {
-                    Console.WriteLine($"NOMATCH: Speech could not be recognized.");
-                }
-                else if (result.Reason == ResultReason.Canceled)
-                {
-                    var cancellation = CancellationDetails.FromResult(result);
-                    Console.WriteLine($"CANCELED: Reason={cancellation.Reason}");
-
-                    if (cancellation.Reason == CancellationReason.Error)
+                    // Checks result.
+                    case ResultReason.RecognizedSpeech:
+                        Console.WriteLine($"We recognized: {result.Text}");
+                        break;
+                    case ResultReason.NoMatch:
+                        Console.WriteLine($"NOMATCH: Speech could not be recognized.");
+                        break;
+                    case ResultReason.Canceled:
                     {
-                        Console.WriteLine($"CANCELED: ErrorCode={cancellation.ErrorCode}");
-                        Console.WriteLine($"CANCELED: ErrorDetails={cancellation.ErrorDetails}");
-                        Console.WriteLine($"CANCELED: Did you update the subscription info?");
+                        var cancellation = CancellationDetails.FromResult(result);
+                        Console.WriteLine($"CANCELED: Reason={cancellation.Reason}");
+
+                        if (cancellation.Reason == CancellationReason.Error)
+                        {
+                            Console.WriteLine($"CANCELED: ErrorCode={cancellation.ErrorCode}");
+                            Console.WriteLine($"CANCELED: ErrorDetails={cancellation.ErrorDetails}");
+                            Console.WriteLine($"CANCELED: Did you update the subscription info?");
+                        }
+
+                        break;
                     }
                 }
             }
